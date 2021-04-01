54356f7eeebea396f9da71433532304e25b74dee
Paola <paolamoraisbg@gmail.com>
2021-04-01 15:26:38 -0300

    Adiçao da rota mensagem

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express()
 app.get('/',function(req,res){
     res.send("Olá Paola!")
 
+}) 
+
+app.get('/mensagem',function(req,res){
+    res.send("Essa mensagem é automática!")
+
 })
+
 
 app.listen(3000,function(){
     console.log("Conexão inicializada")
